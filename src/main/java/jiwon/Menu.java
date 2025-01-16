7d258cb53b43a69f14de7329024fe82c43f7e18b
jiwon0226 <ppg0226@naver.com>
2025-01-17 04:12:20 +0900

    jiwon 작업 끝

--- src/main/java/jiwon/Menu.java
+++ src/main/java/jiwon/Menu.java
@@ -11,6 +11,7 @@ import lombok.ToString;
 public class Menu {
     private String menuId;       // 메뉴 ID
     private String menuName;     // 메뉴 이름
+    private String menuType;     // 메뉴 타입
     private String description;  // 메뉴 설명 (간략한 내용)
     private int price;           // 가격
     private int calorie;         // 칼로리
