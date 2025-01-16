0fb319c76fd37ae23acbc214ec694e738ce35432
sung-yeop <jjabc9459@naver.com>
2025-01-16 16:09:49 +0900

    feat: 포스트 등록 / 삭제 기능 구현

--- src/main/java/sungyeop/Board.java
+++ src/main/java/sungyeop/Board.java
@@ -1,29 +1,64 @@
 package sungyeop;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Board {
     private List<Post> posts = new ArrayList<>();
+    private Map<Post, Integer> postStatistics = new HashMap<>();
+    private static int postNumber = 1;
+
+    private static void printPost(Post post) {
+        System.out.print("\n##########################################\n");
+        System.out.println("Post # " + post.getPostNumber());
+        System.out.println("포스트 제목 : " + post.getTitle());
+        System.out.println("포스트 작성 날짜 : " + post.getDate());
+        System.out.println("포스트 작성자 : " + post.getAuthor());
+        System.out.println("포스트 내용 : " + post.getContent());
+        System.out.println("##########################################");
+    }
 
     public void addPost(Scanner scanner) {
-        System.out.printf("포스트 제목을 입력해주세요 : ");
+        System.out.print("포스트 제목을 입력해주세요 : ");
         String title = scanner.nextLine();
 
-        System.out.printf("포스트 작성 날짜를 입력해주세요 (YYYY-MM-DD) : ");
-        String date = scanner.nextLine();
+        System.out.print("포스트 작성 날짜를 입력해주세요 (YYYY-MM-DD) : ");
+        String date;
+        while(true) {
+            date = scanner.nextLine();
+            if(date.matches("\\d{4}-\\d{2}-\\d{2}")) {
+                break;
+            }
+            System.out.println("날짜 형식을 맞춰서 다시 입력해주세요");
+        }
 
-        System.out.printf("포스트 작성자를 입력해주세요 : ");
+        System.out.print("포스트 작성자를 입력해주세요 : ");
         String author = scanner.nextLine();
 
         System.out.println("포스트 내용을 입력해주세요");
-        String content = scanner.next();
+        String content = scanner.nextLine();
 
-        Post post = new Post(title, date, author, content);
-        System.out.println(post);
+        Post post = new Post(title, content, author, date, postNumber++);
+        posts.add(post);
+        postStatistics.put(post, 0);
+    }
+
+    public void printAllPosts() {
+        if(posts.isEmpty()){
+            System.out.println("등록된 포스트가 없습니다.");
+            return;
+        }
+        posts.forEach(Board::printPost);
+    }
+
+    public void deletePost(Scanner scanner) {
+        System.out.print("삭제하고 싶은 포스트명 혹은 포스트 번호를 말씀해주세요 : ");
+        String input = scanner.nextLine();
+
+        if(input.matches("\\d+")) {
+            int postNumber = Integer.parseInt(input);
+            posts.removeIf(post -> post.getPostNumber() == postNumber);
+            return;
+        }
+        posts.removeIf(post -> post.getTitle().equals(input));
     }
 }
--- src/main/java/sungyeop/Post.java
+++ src/main/java/sungyeop/Post.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 @AllArgsConstructor
@@ -14,11 +15,43 @@ public class Post {
     private String content;
     private String author;
     private String date;
+    private int postNumber;
 
     public static void main(String[] args) {
         Board board = new Board();
         Scanner scanner = new Scanner(System.in);
-        board.addPost(scanner);
+
+        while(true){
+            System.out.println("\n------------------------------------");
+            System.out.println("1번 : 포스트 전체 출력");
+            System.out.println("2번 : 포스트 등록");
+            System.out.println("3번 : 포스트 삭제");
+            System.out.println("6번 : 나가기");
+            System.out.println("------------------------------------\n");
+            System.out.print("번호를 입력해주세요 : ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println();
+
+            switch (choice) {
+                case 1:
+                    board.printAllPosts();
+                    break;
+                case 2 :
+                    board.addPost(scanner);
+                    break;
+                case 3:
+                    board.deletePost(scanner);
+                    break;
+                case 6:
+                    return;
+                default:
+                    System.out.println("잘못 입력");
+                    break;
+            }
+        }
+
+
 
     }
 }
